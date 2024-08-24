6cfb303b9beb6b1b0c9e299cd1fa021bd2608392
StudenikinNikolay <165457990+StudenikinNikolay@users.noreply.github.com>
2024-08-24 21:15:50 +0300

    Update Server.java

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -12,15 +12,17 @@ public class Server {
         System.out.println("Сервер запущен на порту " + port);
 
         while (true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Новое соединение принято от клиента с портом " + clientSocket.getPort());
+            try (Socket clientSocket = serverSocket.accept()) {
+                System.out.println("Новое соединение принято от клиента с портом " + clientSocket.getPort());
 
-            try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-                    final String name = in.readLine();
-                    out.println(String.format("Здравствуйте, %s! Ваш порт: %d", name, clientSocket.getPort()));
+                try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+                    try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                        final String name = in.readLine();
+                        out.println(String.format("Здравствуйте, %s! Ваш порт: %d", name, clientSocket.getPort()));
+                    }
                 }
             }
         }
     }
 }
+
