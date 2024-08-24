05347403ec9f4971a90904e9d7aaa073c5a2b4d0
StudenikinNikolay <165457990+StudenikinNikolay@users.noreply.github.com>
2024-08-24 17:34:30 +0300

    Add files via upload

---
+++ src/main/java/Server.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = 8080; // Порт, на котором будет слушать сервер
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Сервер запущен на порту " + port);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Новое соединение принято от клиента с портом " + clientSocket.getPort());
+
+            try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    final String name = in.readLine();
+                    out.println(String.format("Здравствуйте, %s! Ваш порт: %d", name, clientSocket.getPort()));
+                }
+            }
+        }
+    }
+}
