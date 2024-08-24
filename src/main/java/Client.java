3639074f13c6ff661a2ebcb737d367ebafba60e6
StudenikinNikolay <165457990+StudenikinNikolay@users.noreply.github.com>
2024-08-24 21:15:10 +0300

    Update Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,14 +7,14 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 8080);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            try (Socket socket = new Socket("localhost", 8080)) {
+                try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-            out.println("Client");
-            System.out.println(in.readLine());
-
-            socket.close();
+    out.println("Client");
+                    System.out.println(in.readLine());
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
